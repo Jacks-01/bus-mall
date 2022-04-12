19bfacffb581db92157ae4a61a555c10a730c896
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-04-11 20:38:43 -0500

    started array, not working

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # wireframe
+
+1. Create a constructor function that contains:
+    - name of product
+    - file path of image
+    - times the image has been shown
+
+2. Create an algorithm to randomly generate 3 images 
+
+3. display 3 images side by side in the same spot on refresh
+
+4. increment (x) the properties of the object each time the picture is clicked.
+
+5. attatch an event listener to the images to add the increment 
+
---
+++ js/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+
+
+
+
+
+let randomImage = ['images\bag.jpg', 'boots.jpg'];
+
+
+// function getRandomImage(){
+
+
+
+//   let randomImage = new Array(
+
+//   randomImage[0] = 'images\bag.jpg',
+//   randomImage[1] = 'images\banana.jpg',
+//   randomImage[2] = 'images\bathroom.jpg',
+//   randomImage[3] = 'images\boots.jpg',
+// //   randomImage[4] = 
+// // //   randomImage[5];
+// // //   randomImage[6];
+// // //   randomImage[7];
+// // //   randomImage[8];
+// // //   randomImage[9];
+// // //   randomImage[10];
+// // //   randomImage[11];
+  
+// }
+
+// getRandomImage();
